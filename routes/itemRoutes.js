d2d0a20ba9f4583e3a8dfaabe8204b6c10264cec
anshgupta2498 <anshgupta1298@gmail.com>
2022-01-10 01:30:46 +0530

    itemRouter

--- routes/itemRoutes.js
+++ routes/itemRoutes.js
@@ -3,12 +3,6 @@ const itemRouter = express.Router();
 const itemController = require('./../controllers/itemController')
 const authController = require('./../controllers/authController')
 
-itemRouter.get('/', (req, res, next) => {
-        return res.json({
-            name: "Ansh"
-        })
-    })
-    // itemRouter.get('/item/:id', itemController.getItemById)
 itemRouter.get('/item/:id', itemController.getItemById)
 itemRouter.post('/', itemController.getCategorizedItems)
 itemRouter.get('/', itemController.getAllItems)
